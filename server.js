a462f9320ce58f74a159f8324f7ec25ee679bb32
rsrakib19472195 <rakibpro63@gmail.com>
2026-09-21 14:47:58 +0600

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 // ============================================================
 // CONFIG
 // ============================================================
